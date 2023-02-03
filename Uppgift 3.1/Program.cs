918689dbb74d3f6b65beea5c67d33f8acc1b1e97
Anton <>
2023-02-03 08:57:13 +0100

    Add project files.

---
+++ Uppgift 3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur gammal är du? : ");
+            int ålder = Convert.ToInt32(Console.ReadLine());
+
+            if (ålder >= 50 ) 
+            {
+                Console.WriteLine("Du får vara med i seniormästerskapet");
+            }
+
+            else
+            {
+                Console.WriteLine("Du får inte vara med i seniormästerskapet");
+            }
+
+
+
+        }
+    }
+}
